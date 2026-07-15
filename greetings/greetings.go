c439713dc4b399462fba3e7dc8b507e73e280315
marcelo-sjr <marcelodasilvajr2@gmail.com>
2026-07-15 16:44:08 -0300

    greetings.Hello() now return an error

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -1,9 +1,14 @@
 package greetings
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
-func Hello(name string) string{
+func Hello(name string) (string, error) {
+	if name == "" {
+		return "", errors.New("name argument can't be empty!")
+	}
 	message := fmt.Sprintf("Hello, %s. Welcome!", name)
-	return message
+	return message, nil
 }
-
