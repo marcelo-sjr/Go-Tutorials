9e9da51b7a269bb2e23cfde6cbbc51f568e150f4
marcelo-sjr <marcelodasilvajr2@gmail.com>
2026-07-15 18:44:06 -0300

    randomFormat returns a random string format greeting

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -3,12 +3,42 @@ package greetings
 import (
 	"errors"
 	"fmt"
+	"math/rand"
 )
 
 func Hello(name string) (string, error) {
 	if name == "" {
 		return "", errors.New("name argument can't be empty!")
 	}
-	message := fmt.Sprintf("Hello, %s. Welcome!", name)
+	message := fmt.Sprintf(randomFormat(), name)
 	return message, nil
+}
+
+//starting functions with lowercase turns it into a non exported func(not visible outside package)
+func randomFormat() string {
+	possibleGreetings := []string{
+		"Hi, %v. Welcome!",
+        "Great to see you, %v!",
+        "Hail, %v! Well met!",
+		"Hey, %v! Nice to see you!",
+		"Welcome, %v!",
+		"Greetings, %v!",
+		"Good to have you here, %v!",
+		"It's great to see you, %v!",
+		"Hope you're doing well, %v!",
+		"Howdy, %v!",
+		"Good day, %v!",
+		"Cheers, %v!",
+		"Welcome aboard, %v!",
+		"Happy to see you, %v!",
+		"Hi there, %v!",
+		"Hello and welcome, %v!",
+		"Nice to meet you, %v!",
+		"Hey there, %v!",
+		"Pleasure to have you here, %v!",
+		"Warm greetings, %v!",
+		"Have a wonderful day, %v!",
+	}
+
+	return possibleGreetings[rand.Intn(len(possibleGreetings))]
 }
