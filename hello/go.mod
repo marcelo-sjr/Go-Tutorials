58db3821ff2b5bc1952d85b001b1eff28794909e
marcelo-sjr <marcelodasilvajr2@gmail.com>
2026-07-15 21:05:20 -0300

    Calling greetings.Hellos() from main package

--- hello/go.mod
+++ hello/go.mod
@@ -2,4 +2,4 @@ module github.com/marcelo-sjr/Go-Tutorials/hello
 
 go 1.26.5
 
-require github.com/marcelo-sjr/Go-Tutorials/greetings v0.0.0-20260715214406-9e9da51b7a26
+require github.com/marcelo-sjr/Go-Tutorials/greetings v0.0.0-20260715233237-820cb3467207
--- hello/hello.go
+++ hello/hello.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"fmt"
+	_ "fmt"
 	"log"
-	
+	_ "log"
+
 	"github.com/marcelo-sjr/Go-Tutorials/greetings"
 )
 
@@ -14,4 +16,21 @@ func main() {
 		log.Fatal(err)
 	}
 	fmt.Println(message)
+
+	names := []string{
+		"Alex",
+		"Brian",
+		"Roger",
+		"Melissa",
+		"Emma",
+	}
+
+	messages,err := greetings.Hellos(names)
+	if err != nil{
+		log.Fatal(err)
+	}
+	
+	for _,message := range messages{
+		fmt.Println(message)
+	}
 }
