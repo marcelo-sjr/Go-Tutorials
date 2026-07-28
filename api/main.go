30fe57279b312508a7307ae7a3521b029320ea38
marcelo-sjr <marcelodasilvajr2@gmail.com>
2026-07-28 07:56:50 -0300

    test header and cookie set

--- api/main.go
+++ api/main.go
@@ -1,15 +1,31 @@
 package main
 
 import (
-	_"fmt"
+	_ "fmt"
 	"log"
 	"net/http"
 )
 
+
+
 func main(){
 
+	Cookie := http.Cookie{
+		Name: "Test-Cookie",
+		Value: "Test-Value",
+		Quoted: true,
+		MaxAge: 3600,
+		Secure: true,
+		HttpOnly: true,
+		SameSite: http.SameSiteLaxMode,
+	}
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
+		w.Header().Add("Test", `test-header`)
+		http.SetCookie(w, &Cookie)
+		w.WriteHeader(200)
 		_, err := w.Write([]byte("hello, world!"))
+		//log.Printf("Request: %v\n", r)
 		if err != nil{
 			log.Fatalf("error: %v", err)
 		}
